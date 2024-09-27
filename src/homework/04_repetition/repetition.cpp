c9a42db4cdd4470d4b29a4fb94a97175770de5b0
DSimpson71 <devon.simpson@g.austincc.edu>
2024-09-27 21:41:30 +0000

    Hw 4, strugglin

--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,46 @@
 //add include statements
+#include <iostream>
 
-//add function(s) code here
+//Factorial Function
+int factorial(int num)
+{
+
+    /* (int i = 1; i <= n; ++i) {
+            factorial *= i;
+    */
+
+    auto sum = 1;
+   
+    while(num > 0)
+    {
+       sum = num * sum;
+       num--;
+    }
+    
+    return sum;
+
+}
+//Greatest Common Divisor
+
+int gcd(int num1, int num2)
+{
+    auto a= num1;
+    auto b= num2;
+
+    while(a !=b)
+    {
+        //Swap if num1 is greater than num2
+        if(a<b)
+        {
+            b = num1;
+            a = num2;
+        }
+        //Keep subtracting until a = b 
+        if(a>b)
+        {
+           a -= b; 
+        }
+    }
+    return a;
+
+}
--- test/homework/04_repetition/04_iteration_tests.cpp
+++ test/homework/04_repetition/04_iteration_tests.cpp
@@ -1,7 +1,23 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "repetition.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Verify Factorial Function Works Properly")
+{
+	
+	REQUIRE(factorial(5) == 120);
+	REQUIRE(factorial(3) == 6);
+	REQUIRE(factorial(1) == 1);	
+}
+
+TEST_CASE("Test GCD function works properly") 
+{
+	REQUIRE(gcd(10, 5) == 5);
+ 	//REQUIRE(gcd(40, 15) == 5);
+ 	//REQUIRE(gcd(49, 14) == 7);
+
+}
