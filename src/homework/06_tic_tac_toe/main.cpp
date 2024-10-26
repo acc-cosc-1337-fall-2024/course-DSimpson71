5eb35459bf2c9d82d184b00838b0d61c354ca54b
DSimpson71 <devon.simpson@g.austincc.edu>
2024-10-26 23:13:32 +0000

    HW Complete (TicTacToe)

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,37 @@
+#include "tic_tac_toe.h"
+#include <iostream>
+
+using std::cout; using std::cin; using std::string;
 
 int main() 
 {
+	TicTacToe game;
+	string player_one;
+
+	char option= 'y';
+
+	do {
+		cout<<"Enter first player\n";
+		cin>>player_one;
+
+		game.start_game(player_one);
+		int position;
+		while(!game.game_over())
+		{
+			cout<<"Enter a position\n";
+			cin>>position;
+			game.mark_board(position);
+			game.display_board();
+		}
+
+		cout<<"Do you want to play another game?\nEnter Y for yes or anything else for no\n";
+		cin>> option;
+
+	}while(option == 'y' || option == 'Y');
+
+
+
+
 
 	return 0;
 }
--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,28 @@
 //h
+#include <string>
+#include <vector>
+#include <iostream>
+#ifndef tic_tac_toe_h
+#define tic_tac_toe_h
+
+class TicTacToe
+{
+
+public:
+    bool game_over();
+    void start_game(std::string player_one);
+    void mark_board(int position);
+    std::string get_player() const{return player;};
+    void display_board() const;
+    
+
+private:
+    std::string player;
+    std::vector<std::string> pegs {9, " "};
+    void clear_board(); // Might be public, lets see
+    void set_next_player();
+    bool check_board_full();
+};
+
+
+#endif
