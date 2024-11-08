081f6e5f2e3ce2692fbfe5cd2a12bdd8a5b91db3
DSimpson71 <devon.simpson@g.austincc.edu>
2024-11-08 19:38:48 +0000

    HW 6 Reviewed

--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -22,7 +22,7 @@ void TicTacToe::mark_board(int position)
 
 void TicTacToe::display_board() const
 {
-
+    
     for(long unsigned int i = 0;i < pegs.size();i+=3)
     {
         cout<<pegs[i]<<"|"<<pegs[i+1]<<"|"<<pegs[i+2]<<"\n";
