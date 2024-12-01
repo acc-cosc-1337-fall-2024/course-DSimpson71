62a8b20c30c057760cb58939124ac2ee8dea2d67
DSimpson71 <devon.simpson@g.austincc.edu>
2024-12-01 18:41:53 +0000

    HW9 Progress

--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -10,25 +10,35 @@ class TicTacToe
 {
 
 public:
+    
+   // TicTacToe(int size=3){pegs.resize(size*size, " ");};
+    TicTacToe(int s=3) :pegs(s*s, " "){}; // Remove the number and this text
+
     bool game_over();
     void start_game(std::string player_one);
     void mark_board(int position);
     std::string get_player() const{return player;};
     void display_board() const;
     std::string get_winner() {return winner;};
-    
+   
+
+protected:
+
+    std::vector<std::string> pegs; // {9, " "};
+    virtual bool check_row_win();
+    virtual bool check_column_win();
+    virtual bool check_diagonal_win();
 
 private:
+
     std::string player;
     std::string winner = "C";
-    std::vector<std::string> pegs {9, " "};
+    
     void clear_board(); // Might be public, lets see
     void set_next_player();
     bool check_board_full();	
     
-    bool check_row_win();
-    bool check_column_win();
-    bool check_diagonal_win();
+    
     //void set_winner(){if(player== "X"){winner= "O";} else{winner= "X";}}; <<< old code
     void set_winner();
 
--- src/homework/06_tic_tac_toe/tic_tac_toe_3.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe_3.h
@@ -1,1 +1,46 @@
 //h
+#include <string>
+#include <vector>
+#include <iostream>
+#include "tic_tac_toe_manager.h"
+#include "tic_tac_toe.h"
+#ifndef tic_tac_toe_h_3
+#define tic_tac_toe_h_3
+
+class TicTacToe3 : TicTacToe
+{
+    public:
+
+
+
+
+    protected:
+
+
+
+
+    private:
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
